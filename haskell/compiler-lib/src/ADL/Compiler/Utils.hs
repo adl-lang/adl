298cef5f9d6caef1a60bb83513bfd31aad1c2fdf
Tim Docker <tim@dockerz.net>
2014-02-12 22:31:13 +1100

    Fix problem with lazy file read and --no-overwrite

--- haskell/compiler-lib/src/ADL/Compiler/Utils.hs
+++ haskell/compiler-lib/src/ADL/Compiler/Utils.hs
@@ -9,6 +9,7 @@ import Control.Exception
 import System.FilePath
 import System.Directory(createDirectoryIfMissing)
 import qualified Data.ByteString.Lazy as LBS
+import qualified Data.ByteString as BS
 
 data OutputArgs = OutputArgs {
   oa_log :: String -> IO (),
@@ -24,8 +25,8 @@ writeOutputFile (oa@OutputArgs{oa_noOverwrite=False}) fpath0 t = do
   LBS.writeFile fpath t
 writeOutputFile (oa@OutputArgs{oa_noOverwrite=True}) fpath0 t = do
   let fpath = oa_outputPath oa </> fpath0
-  t0 <- catch (LBS.readFile fpath) ((\_ -> return LBS.empty) :: IOError -> IO LBS.ByteString)
-  if t /= t0
+  t0 <- catch (BS.readFile fpath) ((\_ -> return BS.empty) :: IOError -> IO BS.ByteString)
+  if t /= LBS.fromStrict t0
      then do
        oa_log oa ("writing " ++ fpath ++ ".")
        createDirectoryIfMissing True (takeDirectory fpath)
