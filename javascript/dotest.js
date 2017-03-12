d54e5d3c333649d04d9d62b88f5dcbf659de16cd
Tim Docker <tim@dockerz.net>
2017-03-13 08:25:30 +1100

    Removed vestigial js code from original js backend implementation

--- javascript/dotest.js
+++
@@ -1,7 +0,0 @@
-var adlast = require("sys/adlast.js");
-var test = require("test.js");
-
-console.log(adlast.Module);
-console.log(adlast.DeclType);
-console.log(adlast.DeclType.fields[0][1]());
-console.log(test.Y2);
