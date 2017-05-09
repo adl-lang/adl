aab89cc25e0bd18d35ee8ddbcae84b1ee369d111
Tim Docker <tim@dockerz.net>
2017-05-10 08:30:09 +1000

    Added java helper functions to reduce boilerplate in working with ADL values and json strings

---
+++ java/runtime/src/main/java/org/adl/runtime/JsonHelpers.java
@@ -0,0 +1,39 @@
+package org.adl.runtime;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Simple functions to reduce boilerplate in working with json for ADL values
+ */
+public class JsonHelpers
+{
+  static Gson gson = new GsonBuilder()
+    .serializeNulls()
+    .create();
+
+  static Gson lenientGson = new GsonBuilder()
+    .serializeNulls()
+    .setLenient()
+    .create();
+
+  /** 
+   * Read an ADL value from a json file
+   */
+  public static <T> T fromFile(JsonBinding<T> binding, String path) throws IOException {
+    return binding.fromJson(gson.fromJson(new FileReader(path), JsonElement.class));
+  }
+
+  /** 
+   * Read an ADL value from a leniently formatted json file.
+   *
+   * (See `setLenient` at
+   *  https://static.javadoc.io/com.google.code.gson/gson/2.6.2/com/google/gson/stream/JsonReader.html)
+   */
+  public static <T> T fromLenientFile(JsonBinding<T> binding, String path) throws IOException {
+    return binding.fromJson(lenientGson.fromJson(new FileReader(path), JsonElement.class));
+  }
+};
