a67a4b6d6a90cf1e5a4a0e0f9da2b23d69814076
Tim Docker <tim@dockerz.net>
2017-05-10 08:30:09 +1000

    Added java helper functions to reduce boilerplate in working with ADL values and json strings

--- java/runtime/src/main/java/org/adl/runtime/JsonBinding.java
+++ java/runtime/src/main/java/org/adl/runtime/JsonBinding.java
@@ -10,7 +10,15 @@ import java.io.IOException;
 public interface JsonBinding<T>
 {
   Factory<T> factory();
-  JsonElement toJson( T value );
-  T fromJson( JsonElement json ); 
+  JsonElement toJson(T value);
+  T fromJson(JsonElement json);
+
+  default String toJsonString(T value) {
+    return JsonHelpers.gson.toJson(toJson(value));
+  }
+
+  default T fromJsonString(String str) {
+    return fromJson(JsonHelpers.gson.fromJson(str, JsonElement.class));
+  }
 };
 
