7065b6884aa1245066aec4db40ef236f207a650e
Tim Docker <tim@dockerz.net>
2017-05-26 08:36:54 +1000

    Extended the typescript unit tests to cover unions and field serialization names

--- typescript/example.spec.ts
+++ typescript/example.spec.ts
@@ -44,6 +44,8 @@ describe('Person Structure', () => {
   });
 });
 
+//----------------------------------------------------------------------
+
 const intTreeJsonBinding : JsonBinding<example.IntTree> = createJsonBinding(dresolver,example.texprIntTree());
 
 const tree1 : example.IntTree = {
@@ -68,7 +70,51 @@ describe('IntTree Recursive Type', () => {
     expect(tree2.children[0].value).toEqual(15);
     expect(tree2.children[1].value).toEqual(33);
   })
+  it( 'uses custom serialized names', () => {
+    const json = intTreeJsonBinding.toJson(tree1);
+    expect(json.v).toEqual(7);
+    expect(json.cs[0].v).toEqual(15);
+    expect(json.cs[1].v).toEqual(33);
+  })
 });
+
+//----------------------------------------------------------------------
+
+const dataSourceJsonBinding : JsonBinding<example.DataSource>
+  = createJsonBinding(dresolver,example.texprDataSource());
+
+const dataSource1 : example.DataSource1 = {
+  kind : "implicit"
+};
+
+const dataSource2 : example.DataSource1 = {
+  kind : "inline"
+  value : {
+    encoding : "utf-8",
+    content : "Now is the time, or so they said"
+  }
+};
+
+const dataSource3 : example.DataSource1 = {
+  kind : "file"
+  value : "/tmp/testdata.txt"
+};
+
+describe('DataSource union', () => {
+  it( 'roundtrips via json', () => {
+    const jb = dataSourceJsonBinding;
+    const dataSource1r = jb.fromJson(jb.toJson(dataSource1));
+    expect(dataSource1r.kind).toEqual("implicit");
+    const dataSource2r = jb.fromJson(jb.toJson(dataSource2));
+    expect(dataSource2r.kind).toEqual("inline");
+    expect(dataSource2r.value.encoding).toEqual("utf-8");
+    const dataSource3r = jb.fromJson(jb.toJson(dataSource3));
+    expect(dataSource3r.kind).toEqual("file");
+    expect(dataSource3r.value).toEqual("/tmp/testdata.txt");
+  });
+});
+
+//----------------------------------------------------------------------
 
 const genderTreeJsonBinding : JsonBinding<example.Tree<example.Gender>>
     = createJsonBinding(dresolver,example.texprTree(example.texprGender()));
