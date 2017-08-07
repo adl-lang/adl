a486af6a9b2ffb33b7d30d2c024cbba9358adf49
Tim Docker <tim@dockerz.net>
2017-08-07 23:12:25 +1000

    Moved typescript tests into a subdirectory

--- typescript/tests/run-tests.sh
+++ typescript/tests/run-tests.sh
@@ -4,14 +4,14 @@ set -e
 
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 GENDIR=$HERE/build
-HASKELLDIR=$HERE/../haskell
+HASKELLDIR=$HERE/../../haskell
 ADLSTDLIBDIR=$(cd $HASKELLDIR; stack exec adlc -- show --adlstdlib)
 
 rm -rf $GENDIR
 mkdir -p $GENDIR
 
 # Build ADL and dependencies setup
-(cd $HERE/../haskell; stack exec adlc -- typescript -I $ADLSTDLIBDIR -O $GENDIR --include-rt --runtime-dir $GENDIR/runtime $HERE/example.adl $ADLSTDLIBDIR/sys/types.adl $ADLSTDLIBDIR/sys/adlast.adl $ADLSTDLIBDIR/sys/dynamic.adl)
+(cd $HASKELLDIR; stack exec adlc -- typescript -I $ADLSTDLIBDIR -O $GENDIR --include-rt --runtime-dir $GENDIR/runtime $HERE/example.adl $ADLSTDLIBDIR/sys/types.adl $ADLSTDLIBDIR/sys/adlast.adl $ADLSTDLIBDIR/sys/dynamic.adl)
 yarn
 
 # Compile everything to check all the types, as running jest-ts doesn't actually do so :-(
