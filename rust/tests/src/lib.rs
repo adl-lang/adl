e68a8711bdcac1e6e8e4b69db811010a721828cb
Vasily Chekalkin <bacek@bacek.com>
2020-06-11 20:45:53 +1000

    Run rustfmt on rust code

--- rust/tests/src/lib.rs
+++ rust/tests/src/lib.rs
@@ -1,15 +1,15 @@
+pub mod adlrt;
 pub mod demo1;
 pub mod test1;
+pub mod test14;
+pub mod test18;
 pub mod test2;
+pub mod test20;
 pub mod test3;
 pub mod test4;
 pub mod test5;
 pub mod test6;
 pub mod test7;
-pub mod test14;
-pub mod test18;
-pub mod test20;
-pub mod adlrt;
 
 #[cfg(test)]
 mod tests {
