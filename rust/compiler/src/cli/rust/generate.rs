d3fee8772f87382fc8fbef4d18051395412da067
Tim Docker <tim@dockerz.net>
2023-03-23 10:03:36 +1100

    more codegen

--- rust/compiler/src/cli/rust/generate.rs
+++ rust/compiler/src/cli/rust/generate.rs
@@ -1,10 +1,11 @@
 use super::RustOpts;
 use std::collections::{HashMap, HashSet};
 use std::path::PathBuf;
+use convert_case::{Case, Casing};
 
 
 use crate::adlgen::sys::adlast2::{self as adlast, PrimitiveType};
-use crate::processing::resolver::{Module1, Resolver, TypeExpr1};
+use crate::processing::resolver::{Module1, Resolver, TypeExpr1, TypeRef};
 use crate::processing::writer::TreeWriter;
 
 use crate::cli::rust::rsfile::RSFile;
@@ -44,7 +45,9 @@ pub fn gen_rs_mod_files(opts: &RustOpts, resolver: &Resolver, writer: &mut TreeW
 }
 
 pub fn gen_module(m: &Module1) -> anyhow::Result<String> {
-    let mut out = RSFile::new();
+    let mut out = RSFile::new(m.name.clone());
+    out.import("serde", "Deserialize");
+    out.import("serde", "Serialize");
 
     for d in m.decls.iter() {
         match &d.r#type {
@@ -62,15 +65,41 @@ fn gen_struct(
     s: &adlast::Struct<TypeExpr1>,
     out: &mut RSFile,
 ) -> anyhow::Result<()> {
-    fmtln!(out, "pub struct {} {{", d.name);
-    for f in &s.fields {
-        fmtln!(out, "    {}: {};", f.name, gen_type_expr(&f.type_expr));
+    let efields: Vec<ExtendedField> = s.fields
+    .iter()
+    .map(|field| ExtendedField::new(field, out))
+    .collect();
+
+    let ctor_params: String = efields
+    .iter()
+    .map(|ef| format!("{}: {}", ef.rs_field_name, ef.type_str))
+    .collect::<Vec<String>>()
+    .join(", ");
+
+    let type_params = fmt_type_params(&s.type_params);
+
+    if let Some(serde_json::Value::String(doc)) = d.annotations.0.get(&ann_doc()) {
+      out.pushlns(fmt_doc_comment(doc));
+    }
+
+    fmtln!(out, "pub struct {}{} {{", d.name, type_params );
+    for (i,ef) in efields.iter().enumerate() {
+        if i != 0 {
+          fmtln!(out, "");
+        }
+        if ef.rs_field_name != ef.field.serialized_name {
+          fmtln!(out, "  #[serde(rename=\"{}\")]", ef.field.serialized_name);
+        }
+        fmtln!(out, "  pub {}: {},", ef.rs_field_name, ef.type_str);
     }
     fmtln!(out, "}}");
     fmtln!(out, "");
-    fmtln!(out, "impl {} {{", d.name);
-    fmtln!(out, "  pub fn new() -> {} {{", d.name);
+    fmtln!(out, "impl{} {}{} {{", type_params, d.name, type_params);
+    fmtln!(out, "  pub fn new({}) -> {}{} {{", ctor_params, d.name, type_params);
     fmtln!(out, "    {} {{", d.name);
+    for ef in &efields {
+      fmtln!(out, "      {}: {},",  ef.rs_field_name,  ef.rs_field_name);
+    }
     fmtln!(out, "    }}");
     fmtln!(out, "  }}");
     fmtln!(out, "}}");
@@ -78,35 +107,119 @@ fn gen_struct(
     Ok(())
 }
 
-fn gen_type_expr(te: &TypeExpr1) -> String {
-    match &te.type_ref {
-        adlast::TypeRef::LocalName(_ln) => "UNIMP.LOCAL_NAME".to_owned(),
-        adlast::TypeRef::ScopedName(_sn) => "UNIMP.SCOPED_NAME".to_owned(),
-        adlast::TypeRef::Primitive(p) => gen_primitive_type_expr(p, &te.parameters),
-        adlast::TypeRef::TypeParam(_t) => "UNIMP.TYPE_PARAM".to_owned(),
-    }
+struct ExtendedField<'a> {
+  field: &'a adlast::Field<TypeExpr1>,
+  rs_field_name: String,
+  type_str: String,
+  type_expr_gen: TypeExprGen,
 }
 
-fn gen_primitive_type_expr(p: &PrimitiveType, _params: &[TypeExpr1]) -> String {
-    match p {
-        PrimitiveType::Void => "()".to_owned(),
-        PrimitiveType::Bool => "bool".to_owned(),
-        PrimitiveType::Int8 => "i8".to_owned(),
-        PrimitiveType::Int16 => "i16".to_owned(),
-        PrimitiveType::Int32 => "i32".to_owned(),
-        PrimitiveType::Int64 => "i64".to_owned(),
-        PrimitiveType::Word8 => "u8".to_owned(),
-        PrimitiveType::Word16 => "u16".to_owned(),
-        PrimitiveType::Word32 => "u32".to_owned(),
-        PrimitiveType::Word64 => "u64".to_owned(),
-        PrimitiveType::Float => "f32".to_owned(),
-        PrimitiveType::Double => "f64".to_owned(),
-        PrimitiveType::Json => "UNIMP_JSON".to_owned(),
-        PrimitiveType::ByteVector => "Vec<u8>".to_owned(),
-        PrimitiveType::String => "String".to_owned(),
-        PrimitiveType::Vector => "UNIMP_VECTOR".to_owned(),
-        PrimitiveType::StringMap => "UNIMP_STRINGMAP".to_owned(),
-        PrimitiveType::Nullable => "UNIMP_NULLABLE".to_owned(),
-        PrimitiveType::TypeToken => "UNIMP_TYPETOKEN".to_owned(),
+impl <'a> ExtendedField<'a> {
+  fn new(field: &'a adlast::Field<TypeExpr1>, out: &mut RSFile) -> Self {
+    let type_expr_gen = TypeExprGen::new(&field.type_expr);
+    let type_str = type_expr_gen.gen_type(out);
+    let rs_field_name = field.name.to_case(Case::Snake);
+    ExtendedField{field, rs_field_name, type_str, type_expr_gen}
+  }
+}
+
+trait TypeCodeGen {
+  fn gen_type(&self, rsfile: &mut RSFile) -> String;
+}
+
+struct TypeExprGen {
+  type_ref:  adlast::TypeRef,
+  params: Vec<Box<dyn TypeCodeGen>>,
+}
+
+impl TypeExprGen {
+  fn new(type_expr: &TypeExpr1) -> Self {
+    let params = type_expr.parameters
+        .iter()
+        .map(|te| Box::new(TypeExprGen::new(te)) as Box<dyn TypeCodeGen>)
+        .collect();
+    TypeExprGen {
+      type_ref: type_expr.type_ref.clone(),
+      params,
     }
+  }
+}
+
+impl TypeCodeGen for TypeExprGen {
+  fn gen_type(&self, rsfile: &mut RSFile) -> String {
+
+    let params: Vec<String> = self.params
+    .iter()
+    .map(|p| p.gen_type(rsfile))
+    .collect();
+
+    match &self.type_ref {
+      adlast::TypeRef::LocalName(ln) => format!("{}{}", ln,fmt_type_params(&params)),
+      adlast::TypeRef::ScopedName(sn) => {
+        let name = rsfile.import(&sn.module_name, &sn.name);
+        format!("{}{}", name, fmt_type_params(&params))
+      },
+      adlast::TypeRef::Primitive(p) => {
+        match p {
+          PrimitiveType::Void => "()".to_owned(),
+          PrimitiveType::Bool => "bool".to_owned(),
+          PrimitiveType::Int8 => "i8".to_owned(),
+          PrimitiveType::Int16 => "i16".to_owned(),
+          PrimitiveType::Int32 => "i32".to_owned(),
+          PrimitiveType::Int64 => "i64".to_owned(),
+          PrimitiveType::Word8 => "u8".to_owned(),
+          PrimitiveType::Word16 => "u16".to_owned(),
+          PrimitiveType::Word32 => "u32".to_owned(),
+          PrimitiveType::Word64 => "u64".to_owned(),
+          PrimitiveType::Float => "f32".to_owned(),
+          PrimitiveType::Double => "f64".to_owned(),
+          PrimitiveType::Json => {
+            rsfile.import("serde_json", "Value")
+          },  
+          PrimitiveType::ByteVector => "Vec<u8>".to_owned(),
+          PrimitiveType::String => "String".to_owned(),
+          PrimitiveType::Vector => format!("Vec{}", fmt_type_params(&params)),
+          PrimitiveType::StringMap => {
+            let hash_map = rsfile.import("std::collections", "StringMap");
+            format!("{}<String, {}>", hash_map, params.join(","))
+          }
+          PrimitiveType::Nullable => format!("Option{}", fmt_type_params(&params)),
+          PrimitiveType::TypeToken => {
+            let phantom_data = rsfile.import("std::marker", "PhantomData");
+            phantom_data
+          }
+        }
+      }, 
+      adlast::TypeRef::TypeParam(t) => t.to_string(),
+    }
+  }
+}
+
+
+fn fmt_type_params(params: &[String]) -> String {
+  if params.len() == 0 {
+    "".to_owned()
+  } else {
+    format!("<{}>", params.join("."))
+  }
+}
+
+fn fmt_doc_comment(doc_str: &str) -> Vec<String> {
+  let mut lines = Vec::new();
+  lines.push("/**".to_owned());
+  for s in doc_str.lines() {
+    let s = s.trim();
+    if s != "" {
+      lines.push(format!(" * {}", s))
+    }
+  }
+  lines.push(" */".to_owned());
+  lines
+}
+
+fn ann_doc() ->  adlast::ScopedName {
+  adlast::ScopedName {
+    module_name: "sys.annotations".to_owned(),
+    name: "Doc".to_owned(),
+  }
 }
--- rust/compiler/src/cli/rust/rsfile.rs
+++ rust/compiler/src/cli/rust/rsfile.rs
@@ -1,22 +1,68 @@
+use std::collections::{BTreeSet};
+
 
 /// In memory representation of a rust file during codegen
 pub struct RSFile {
-  lines: Vec<String>,
+  module_name: String,
+  body: Vec<String>,
+  imports: BTreeSet<RustScopedImport>,
+  imported_names: BTreeSet<String>,
+
 }
 
 impl RSFile {
-  pub fn new() -> Self {
+  pub fn new(module_name: String) -> Self {
       RSFile{
-          lines: Vec::new(),
+          module_name,
+          body: Vec::new(),
+          imports: BTreeSet::new(),
+          imported_names: BTreeSet::new(),
       }
   }
 
   pub fn pushln(&mut self, text: String ) {
-      self.lines.push(text);
+      self.body.push(text);
+  }
+
+  pub fn pushlns(&mut self, lines: Vec<String> ) {
+    for l in lines {
+      self.body.push(l);
+    }
+  }
+
+  pub fn import(&mut self, rs_module: &str, rs_name: &str) -> String {
+    let rsi = RustScopedImport{
+      rs_module: rs_module.to_owned(),
+      rs_name: rs_name.to_owned(),
+    };
+    if self.imports.contains(&rsi) {
+      // Use the already imported local name
+      rs_name.to_owned()
+    } else {
+      if self.imported_names.contains(rs_name) {
+        // Use the fully scoped name as the local name is already in use
+        format!("{}::{}", rs_module, rs_name)
+      } else {
+        // Add the import
+        self.imported_names.insert(rs_name.to_owned());
+        self.imports.insert(rsi);
+        rs_name.to_owned()
+      }
+    }
   }
 
   pub fn to_string(&self) -> String {
-      self.lines.join("\n")
+    let mut lines = Vec::new();
+    lines.push(format!("// @generated from adl module {}",self.module_name));
+    lines.push("".to_owned());
+    for rsi in &self.imports {
+      lines.push(format!("use {}::{};", rsi.rs_module, rsi.rs_name));
+    }
+    lines.push("".to_owned());
+    for l in &self.body {
+      lines.push(l.clone())
+    }
+    lines.join("\n")
   }
 }
 
@@ -25,4 +71,10 @@ macro_rules! fmtln {
   ($dst:expr, $($arg:tt)*) => {
       $dst.pushln(format!($($arg)*))
   };
+}
+
+#[derive(Eq,PartialEq,Ord,PartialOrd)]
+struct RustScopedImport {
+  rs_module: String,
+  rs_name: String,
 }
